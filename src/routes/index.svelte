a746d62d15936c5348812745a179c45ce3f0de0c
Ian Harris <ian@fofgof.xyz>
2021-06-20 01:23:23 -0700

    test postMessage for bid

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,25 @@
+<script>
+import { onMount } from 'svelte'
+let bid
+let url = 'https://pleasehelpme.bid'
+
+function openBid() {
+  bid = window.open(url, '_blank')
+
+  setTimeout(() => {
+    bid.postMessage('test', url)
+  }, 3000)
+}
+
+onMount(() => {
+  window.addEventListener('message', (event) => {
+    if (event.origin !== url) return
+
+    console.log({ event })
+  }, false)
+})
+</script>
+
 <p>please help me</p>
+
+<div on:click={ openBid }>test</div>
